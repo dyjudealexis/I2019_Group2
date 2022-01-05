ebec91fcb8b865d5b49f50f00b93975314773672
Jude Alexis Dy <dyjudealexis@gmail.com>
2022-01-05 21:33:10 +0800

    updated!

---
+++ app/src/main/java/com/example/brgyresponseapp/Home.java
@@ -0,0 +1,44 @@
+package com.example.brgyresponseapp;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeF()).commit();
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+
+            switch (item.getItemId()){
+                case R.id.homeM:
+                    fragment = new HomeF();
+                    break;
+                case R.id.adminM:
+                    fragment = new AdminF();
+                    break;
+                case R.id.moreM:
+                    fragment = new MoreF();
+                    break;
+            }
+
+            assert fragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,fragment).commit();
+            return true;
+        });
+    }
+}
